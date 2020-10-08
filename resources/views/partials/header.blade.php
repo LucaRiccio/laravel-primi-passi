a0a8017e9df95f6f50ae5415eb35a3d52da1bad0
LucaRiccio <lucariccio888@gmail.com>
2020-10-09 00:08:23 +0200

    Consegna esercizio (3° tentativo).

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,8 @@
+<header>
+  <img src="https://cdn.shortpixel.ai/client/q_lossy,ret_img,w_250,h_145/https://www.lamolisana.it/wp-content/uploads/2020/05/marchio-sito-test.png" alt="">
+  <nav>
+    <li> <a href="{{ route('home') }}">Home</a> </li>
+    <li> <a href="{{ route('prodotti') }}">Prodotti</a> </li>
+    <li> <a href="{{ route('contatti') }}">Contatti</a> </li>
+  </nav>
+</header>
