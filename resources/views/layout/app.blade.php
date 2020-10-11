72c3c975604249e755b15eee9612135cca1a45fd
LucaRiccio <lucariccio888@gmail.com>
2020-10-11 19:42:55 +0200

    Consegna esercizio (aggiunto footer)

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -14,6 +14,6 @@
       @yield('main')
     </main>
 
-
+    @include('partials.footer')
   </body>
 </html>
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,59 @@
+<footer>
+
+  <div class="logo-footer">
+    <img src="https://cdn.shortpixel.ai/client/q_lossy,ret_img/https://www.lamolisana.it/wp-content/uploads/2020/04/marchio-sito-test.png" alt="">
+  </div>
+
+  <div class="pastificio">
+    <ul>
+      <li><h3>PASTIFICIO</h3></li>
+      <li><a href="#">  Il Pastificio</a></li>
+      <li><a href="#">Grano decorticato a pietra</a></li>
+      <li><a href="#">Il Molise c’è</a></li>
+      <li><a href="#">Filiera Integrata</a></li>
+      <li><a href="#">100 anni di pasta</a></li>
+      <li><a href="#">Sartoria della pasta</a></li>
+    </ul>
+  </div>
+
+  <div class="prodotti">
+    <ul>
+      <li><h3>PRODOTTI</h3></li>
+      <li><a href="#">Le classiche</a></li>
+      <li><a href="#">Le Integrali</a></li>
+      <li><a href="#">Le Speciali</a></li>
+      <li><a href="#">Le biologiche</a></li>
+      <li><a href="#">Le gluten-free</a></li>
+      <li><a href="#">Le sempre</a></li>
+    </ul>
+  </div>
+
+
+  <div class="news">
+    <ul>
+      <li><h3>NEWS</h3></li>
+      <li><a href="#">» E-COMMERCE</a></li>
+      <li><a href="#">» B2B</a></li>
+      <li><a href="#">» COMUNICAZIONE</a></li>
+      <li><a href="#">» SCARICA I CATALOGHI</a></li>
+      <li><a href="#">» CONTATTI</a></li>
+      <li><a href="#">» CERTIFICAZIONI</a></li>
+    </ul>
+  </div>
+
+  <div class="collezione">
+    <ul>
+      <li><h3>COLLEZIONE DA CHEF</h3></li>
+      <li><a href="#">Collezione da chef</a></li>
+      <li><a href="#">Grandi cucine</a></li>
+      <li><a href="#">Biologiche</a></li>
+      <li><a href="#">Quadrate</a></li>
+      <li><a href="#">Grandi cucine</a></li>
+      <li><a href="#">Biologiche</a></li>
+    </ul>
+  </div>
+
+
+
+
+</footer>
