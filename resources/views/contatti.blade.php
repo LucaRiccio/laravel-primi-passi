fb91862950a6e5bc5fb011d4a5084dbeb32dfecd
LucaRiccio <lucariccio888@gmail.com>
2020-10-11 20:31:33 +0200

    Consegna esercizio (aggiunta pag. contatti)

--- resources/views/contatti.blade.php
+++ resources/views/contatti.blade.php
@@ -1,1 +1,34 @@
 @extends('layout.app')
+
+@section('titolo')
+  Contatti
+@endsection
+
+@section('main')
+
+<div class="contatti">
+  <div class="contatti-top">
+    <h3>CONTATTI:</h3>
+    <p>C.da Colle delle Api 100/A</p>
+    <p>86100 CAMPOBASSO</p>
+    <p>numero verde: 800.81.80.81</p>
+    <p>whatsapp: 380.12.92.455</p>
+  </div>
+
+  <div class="contatti-bottom"> <strong></strong>
+    <p><strong>Ragione sociale:</strong> La Molisana S.P.A.</p>
+    <p><strong>Sede legale:</strong> Contrada Colle delle Api, 100/A</p>
+    <p>86100 - Campobasso (CB)</p>
+    <p><strong>Pec:</strong> lamolisana@pec.it</p>
+    <p><strong>Tel:</strong> +39 0874 4981</p>
+    <p><strong>Fax:</strong> +39 0874 629584</p>
+    <p>Per segnalazioni scrivere a:</p>
+    <p>  <a href="#"><strong>info@lamolisana.it</strong></a> </p>
+    <p>Per contattare l'area commerciale:</p>
+    <p> <a href="#"><strong>commerciale@lamolisana.it</strong></a> </p>
+    <p>Per contattare l'area export:</p>
+    <p> <a href="#"><strong>export@lamolisana.it</strong></a> </p>
+  </div>
+</div>
+
+@endsection
